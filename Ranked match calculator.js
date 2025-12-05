aadc0a687fb4f87528feb3f62749f8c7921a8f92
MateusLessa4 <mateuslessa04@gmail.com>
2025-12-05 14:55:17 -0300

    Ranked match calculator

--- Ranked match calculator.js
+++ Ranked match calculator.js
@@ -5,29 +5,35 @@
 let jogador = "MtLessa";
 let vitorias = 99;
 let derrotas = 12;
-let saldoVitorias = vitorias - derrotas;
-let nivel = "";
 
-if (saldoVitorias === 10) {
+function rankedCalculator(vitorias, derrotas) {
+    let saldo = vitorias - derrotas;
+    let nivel = "";
+
+if (saldo === 10) {
     nivel = "Ferro";
 } 
-else if (saldoVitorias <= 11 || saldoVitorias <= 20) {
+else if (saldo <= 11 || saldo <= 20) {
     nivel = "Bronze";
 } 
-else if (saldoVitorias <= 21 || saldoVitorias <= 50) {
+else if (saldo <= 21 || saldo <= 50) {
     nivel = "Prata";
 } 
-else if (saldoVitorias <= 51 || saldoVitorias <= 80) {
+else if (saldo <= 51 || saldo <= 80) {
     nivel = "Ouro";
 } 
-else if (saldoVitorias <= 81 || saldoVitorias <= 90) {
+else if (saldo <= 81 || saldo <= 90) {
     nivel = "Diamante";
 } 
-else if (saldoVitorias <= 91 || saldoVitorias <= 100) {
+else if (saldo <= 91 || saldo <= 100) {
     nivel = "Lendario";
 } 
-else if (saldoVitorias <= 101) {
+else if (saldo <= 101) {
     nivel = "Imortal";
 }
 
-console.log("O jogador " + jogador + ", tem de saldo de: " + saldoVitorias + " vitórias, e está no nível: " + nivel + "!");
+console.log(`O jogador ${jogador}, tem de saldo de: ${saldo} vitórias, e está no nível: ${nivel}!`);
+    return saldo;
+}
+
+let resultado = rankedCalculator(vitorias, derrotas);
